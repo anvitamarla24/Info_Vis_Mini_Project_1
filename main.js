1499bf7aaa8f0eb779e03d346fdb5addb2089c18
anvitamarla24 <56138555+anvitamarla24@users.noreply.github.com>
2020-05-13 14:39:02 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 // Load the datasets and call the functions to make the visualizations
 
 Promise.all([
-  d3.csv('data/scores.csv', d3.autoType),
+  d3.csv('data/data_diff.csv', d3.autoType),
+  d3.csv('DATA_VIS2.csv', d3.autoType),
+  d3.csv('DATA_VIS3.csv', d3.autoType),
   d3.json('data/countries.json'),
-]).then(([data, geoJSON]) => {
+]).then(([data, DATA_VIS2, DATA_VIS3, geoJSON]) => {
   vis1(data, d3.select('#vis1'));
-  vis2(geoJSON, d3.select('#vis2'));
+  vis2(DATA_VIS2, geoJSON, d3.select('#vis2'))
+  vis3(DATA_VIS3, geoJSON, d3.select('#vis3'));
 });
